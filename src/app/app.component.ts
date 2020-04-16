eb99f97f9f1b97a4ea6ad76affc8adb5bda4546b
Gaëtan Bloch <gaetan.bloch@gmail.com>
2020-04-16 11:59:32 +0200

    Added pipe to format the server started date

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,25 +11,25 @@ export class AppComponent {
       instanceType: 'medium',
       name: 'Production Server',
       status: 'critical',
-      started: new Date(1989, 0, 20).toLocaleString()
+      started: new Date(1989, 0, 20)
     },
     {
       instanceType: 'large',
       name: 'User Database',
       status: 'stable',
-      started: new Date(1989, 0, 20).toLocaleString()
+      started: new Date(1989, 0, 20)
     },
     {
       instanceType: 'small',
       name: 'Development Server',
       status: 'offline',
-      started: new Date(1989, 0, 20).toLocaleString()
+      started: new Date(1989, 0, 20)
     },
     {
       instanceType: 'small',
       name: 'Testing Environment Server',
       status: 'stable',
-      started: new Date(1989, 0, 20).toLocaleString()
+      started: new Date(1989, 0, 20)
     }
   ];
   getStatusClasses(server: {instanceType: string, name: string, status: string, started: Date}) {
